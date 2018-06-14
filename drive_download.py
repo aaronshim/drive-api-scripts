765ae2425ea09557808340fe6569a3940e1276d3
Aaron Shim <aaron.k.shim@gmail.com>
2018-06-14 00:13:41 -0400

    Downloading to a file finally works.

--- drive_download.py
+++ drive_download.py
@@ -1,8 +1,11 @@
 from __future__ import print_function
 
-from apiclient import discovery, errors
+from apiclient import discovery, errors, http
 from httplib2 import Http
 from oauth2client import file, client, tools
+from pathlib import Path
+import hashlib
+import io
 import json
 import logging
 import os
@@ -32,18 +35,74 @@ if not creds or creds.invalid:
 DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
 logger.debug('Successfully authenticated.')
 
-# Helper to download the Drive file to a local location.
-
 
 def copy_file(drive_id, location):
-    logger.info("Copying ID %s to %s" % (drive_id, location))
-    # Equivalent to touch.
-    open(location + drive_id, 'a').close()
+    """
+    Helper to download the Drive file to a local location.
+    """
+    # Rename file if a file with the same name already exists.
+    if Path(location).is_file():
+        logger.debug("File already exists at %s. Renaming." % location)
+        location += '.1'
+    # Actual download.
+    logger.debug("Starting copying ID %s to %s" % (drive_id, location))
+    try:
+        request = DRIVE.files().get_media(fileId=drive_id)
+        with io.FileIO(location, 'w') as file:
+            downloader = http.MediaIoBaseDownload(file, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                logger.debug("Download %d%%." % int(status.progress() * 100))
+        logger.info("Copied ID %s to %s" % (drive_id, location))
+        # Check that our copying was correct.
+        check_copied_file(drive_id, location)
+    except errors.HttpError as e:
+        # TODO: Maybe we should have an export option instead for non-binary
+        # types.
+        logger.error(e)
+        # Cleanup.
+        os.remove(location)
+        logger.info("Removing file %s." % location)
 
-# Helper to provide an iterator for files in a given directory.
+
+def check_copied_file(drive_id, location):
+    """
+    Helper to check that a file on disk matches that on Drive.
+    """
+    logger.debug("Starting check of file at %s with ID %s" %
+                 (location, drive_id))
+    # Compute hash and file size of file on Drive.
+    response = DRIVE.files().get(fileId=drive_id, fields='id, md5Checksum, size').execute()
+    if not 'size' in response or not 'md5Checksum' in response:
+        logger.info("File %s with ID %s cannot be checked for size and checksum." % (
+            location, drive_id))
+        return
+    # Compute hash and file size of file on disk.
+    m = hashlib.md5()
+    with open(location, 'rb') as file:
+        m.update(file.read())
+    disk_md5 = m.hexdigest()
+    disk_size = os.path.getsize(location)
+    # Compare.
+    if disk_md5 == response['md5Checksum']:
+        logger.info("Checksum %s matches for file at %s with ID %s" %
+                    (disk_md5, location, drive_id))
+    else:
+        logger.fatal("Checksums do not match for file at %s with ID %s: (%s, %s)" % (
+            location, drive_id, disk_md5, response['md5Checksum']))
+    if disk_size == int(response['size']):
+        logger.info("Size %s matches for file at %s with ID %s" %
+                    (disk_size, location, drive_id))
+    else:
+        logger.fatal("Sizes do not match for file at %s with ID %s: (%s, %s)" % (
+            location, drive_id, disk_size, response['size']))
 
 
 def get_files_in_directory(drive_id):
+    """
+    Helper to provide an iterator for files in a given directory.
+    """
     has_next = True
     request = DRIVE.files().list(pageSize='1000', q="'%s' in parents" %
                                  drive_id, fields='*')
@@ -68,6 +127,7 @@ directory_queue = [(directory_to_start, 'root')]
 
 os.mkdir('root')
 
+# Breadth-first trasversal of our directory structure.
 while len(directory_queue) > 0:
     directory = directory_queue.pop(0)
     for f in get_files_in_directory(directory[0]):
